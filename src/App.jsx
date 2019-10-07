8ef96ca1b9ed4805e1480a2fc5e4ef965562db43
pyBae-py <sajeel.ahmed@protonmail.com>
2019-10-08 00:02:28 +0500

    :laugh: Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.css'
-
+import Header from './components/Header.jsx';
 function App() {
   return (
     <div className="app-wrapper">
-      
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../static/HeaderStyle.css';
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+
+        </div>
+    )
+}
+
+export default Header;
