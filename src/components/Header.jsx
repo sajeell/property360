eb3f7c46018af35879ec03bb5466b468d731160b
pyBae-py <sajeel.ahmed@protonmail.com>
2019-10-08 01:04:11 +0500

    :happy: Header Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../static/HeaderStyle.css';
 const Header = () => {
     return (
         <div className="header-wrapper">
-
+            <div class="logo">
+                <h3>Gulberg Greens</h3>
+            </div>
+            <input type="text" className="search-bar" placeholder="Search Land" />
+            <div className="header-buttons">
+                <button>
+                    Rent
+                </button>
+                <button>
+                    Purchase
+                </button>
+                <button>
+                    Sale
+                </button>
+            </div>
+            <button className="admin-button">
+                Admin Portal
+            </button>
         </div>
+
+
+
+
     )
 }
 
