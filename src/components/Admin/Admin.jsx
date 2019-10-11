7bea311e12cf00e8a8bf0bf30d8a7f2098ae07e5
RealAther <real.ather@gmail.com>
2019-10-11 09:12:13 +0500

    :art: Prettier

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -4,14 +4,13 @@ import ASign from './Admin Sign.jsx'
 // import ADrawer from './Admin Drawer.jsx'
 import AHeader from './Admin Header.jsx'
 const Admin = () => {
-    return (
-        <div className="admin-wrapper">
-            <AHeader />
-            <ASign />
-            {/* <ADrawer /> */}
-        </div>
-    )
+  return (
+    <div className="admin-wrapper">
+      <AHeader />
+      <ASign />
+      {/* <ADrawer /> */}
+    </div>
+  )
 }
 
-
-export default Admin;
+export default Admin
