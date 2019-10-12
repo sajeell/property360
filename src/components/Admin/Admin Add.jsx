959f871272635a983e4d98e7b5e43d57b9f078e1
pyBae-py <sajeel.ahmed@protonmail.com>
2019-10-13 01:01:25 +0500

    :new: Added features into Admin Portal

--- src/components/Admin/Admin Add.jsx
+++ src/components/Admin/Admin Add.jsx
@@ -6,6 +6,8 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
 import Upload from './Admin Upload.jsx';
+import Area from './Admin Area.jsx';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 const useStyles = makeStyles(theme => ({
     wrapper: {
         display: 'flex',
@@ -22,6 +24,9 @@ const useStyles = makeStyles(theme => ({
     formControl: {
         margin: theme.spacing(1),
         minWidth: 120,
+    },
+    text: {
+        fontSize: '12px'
     },
 }));
 
@@ -68,98 +73,103 @@ export default function Add() {
     const handleOpenType = () => {
         setOpenType(true);
     };
-
-
     return (
         <div className={classes.wrapper}>
+            <Router>
 
-            <form autoComplete="off">
-                {/* <Button className={classes.button} onClick={handleOpen}>
+                <form autoComplete="off">
+                    {/* <Button className={classes.button} onClick={handleOpen}>
                 Open the select
             </Button> */}
-                <FormControl className={classes.formControl}>
-                    <InputLabel htmlFor="demo-controlled-open-select">Choose Area:</InputLabel>
-                    <Select
-                        open={openArea}
-                        onClose={handleCloseArea}
-                        onOpen={handleOpenArea}
-                        value={area}
-                        onChange={handleChangeArea}
-                        inputProps={{
-                            name: 'area',
-                            id: 'demo-controlled-open-select',
-                        }}
-                    >
-                        <MenuItem value="">
-                            <em>None</em>
-                        </MenuItem>
-                        <MenuItem value={'G-15'}>G-15</MenuItem>
-                        <MenuItem value={'F-15'}>F-15</MenuItem>
-                        <MenuItem value={'F-16'}>F-16</MenuItem>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel htmlFor="demo-controlled-open-select">Choose Area:</InputLabel>
+                        <Select
+                            open={openArea}
+                            onClose={handleCloseArea}
+                            onOpen={handleOpenArea}
+                            value={area}
+                            onChange={handleChangeArea}
+                            inputProps={{
+                                name: 'area',
+                                id: 'demo-controlled-open-select',
+                            }}
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={'G-15'}>G-15</MenuItem>
+                            <MenuItem value={'F-15'}>F-15</MenuItem>
+                            <MenuItem value={'F-16'}>F-16</MenuItem>
 
-                    </Select>
-                </FormControl>
-            </form>
-            <br />
-            <form autoComplete="off">
-                {/* <Button className={classes.button} onClick={handleOpen}>
+                        </Select>
+                    </FormControl>
+                </form>
+                <br />
+                <form autoComplete="off">
+                    {/* <Button className={classes.button} onClick={handleOpen}>
                 Open the select
             </Button> */}
-                <FormControl className={classes.formControl}>
-                    <InputLabel htmlFor="demo-controlled-open-select">Choose Commerce:</InputLabel>
-                    <Select
-                        open={openCommerce}
-                        onClose={handleCloseCommerce}
-                        onOpen={handleOpenCommerce}
-                        value={commerce}
-                        onChange={handleChangeCommerce}
-                        inputProps={{
-                            name: 'commerce',
-                            id: 'demo-controlled-open-select',
-                        }}
-                    >
-                        <MenuItem value="">
-                            <em>None</em>
-                        </MenuItem>
-                        <MenuItem value={'rent'}>Rent</MenuItem>
-                        <MenuItem value={'sale'}>Sale</MenuItem>
-
-                    </Select>
-                </FormControl>
-            </form>
-            <br />
-            <form autoComplete="off">
-                {/* <Button className={classes.button} onClick={handleOpen}>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel htmlFor="demo-controlled-open-select">Choose Commerce:</InputLabel>
+                        <Select
+                            open={openCommerce}
+                            onClose={handleCloseCommerce}
+                            onOpen={handleOpenCommerce}
+                            value={commerce}
+                            onChange={handleChangeCommerce}
+                            inputProps={{
+                                name: 'commerce',
+                                id: 'demo-controlled-open-select',
+                            }}
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={'rent'}>Rent</MenuItem>
+                            <MenuItem value={'sale'}>Sale</MenuItem>
+                        </Select>
+                    </FormControl>
+                </form>
+                <br />
+                <form autoComplete="off">
+                    {/* <Button className={classes.button} onClick={handleOpen}>
                 Open the select
             </Button> */}
-                <FormControl className={classes.formControl}>
-                    <InputLabel htmlFor="demo-controlled-open-select">Choose Type:</InputLabel>
-                    <Select
-                        open={openType}
-                        onClose={handleCloseType}
-                        onOpen={handleOpenType}
-                        value={type}
-                        onChange={handleChangeType}
-                        inputProps={{
-                            name: 'type',
-                            id: 'demo-controlled-open-select',
-                        }}
-                    >
-                        <MenuItem value="">
-                            <em>None</em>
-                        </MenuItem>
-                        <MenuItem value={'appartment'}>Appartment</MenuItem>
-                        <MenuItem value={'house'}>House</MenuItem>
-                        <MenuItem value={'shop'}>Shop</MenuItem>
-                        <MenuItem value={'plot'}>Plot</MenuItem>
-                    </Select>
-                </FormControl>
-                <Upload />
-                <Button variant="outlined" color="primary" className={classes.button} type="submit">
-                    Add
+
+                    <FormControl className={classes.formControl}>
+                        <InputLabel htmlFor="demo-controlled-open-select">Choose Type:</InputLabel>
+                        <Select
+                            open={openType}
+                            onClose={handleCloseType}
+                            onOpen={handleOpenType}
+                            value={type}
+                            onChange={handleChangeType}
+                            inputProps={{
+                                name: 'type',
+                                id: 'demo-controlled-open-select',
+                            }}
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={'appartment'}>Appartment</MenuItem>
+                            <MenuItem value={'house'}>House</MenuItem>
+                            <MenuItem value={'shop'}>Shop</MenuItem>
+                            <MenuItem value={'plot'}>Plot</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <Upload />
+                    <p className={classes.text}>Want to increase coverage area?  <Link to="/addarea">Click Here!</Link></p>
+                    <Button variant="outlined" color="primary" className={classes.button} type="submit">
+                        Add Property
                 </Button>
-            </form>
-
+                </form>
+                <Switch>
+                    <Route path="/addarea">
+                        <Area />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
