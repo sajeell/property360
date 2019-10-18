6e6e9bede4a3e36eeae2c1a0abf07a1c27291da2
pyBae-py <sajeel.ahmed@protonmail.com>
2019-10-19 02:42:48 +0500

    :lipstick: UI Fixes

--- src/components/Admin/AdminAdd.jsx
+++ src/components/Admin/AdminAdd.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import gql from 'graphql-tag.macro'
-import { makeStyles } from '@material-ui/core/styles'
-import InputLabel from '@material-ui/core/InputLabel'
-import MenuItem from '@material-ui/core/MenuItem'
-import FormControl from '@material-ui/core/FormControl'
-import Select from '@material-ui/core/Select'
-import Button from '@material-ui/core/Button'
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import React from "react";
+import gql from "graphql-tag.macro";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import Upload from './AdminUpload.jsx'
-import Area from './AdminArea.jsx'
-import { useQuery, useMutation } from '../../apollo'
+import Upload from "./AdminUpload.jsx";
+import Area from "./AdminArea.jsx";
+import { useQuery, useMutation } from "../../apollo";
 
 const ADMIN_ADD_QUERY = gql`
   query AdminAddQuery {
@@ -23,7 +23,7 @@ const ADMIN_ADD_QUERY = gql`
       name
     }
   }
-`
+`;
 
 const ADD_LISTING_MUTATION = gql`
   mutation($input: AddListingMutationInput!) {
@@ -39,112 +39,114 @@ const ADD_LISTING_MUTATION = gql`
       }
     }
   }
-`
+`;
 
 const useStyles = makeStyles(theme => ({
   wrapper: {
-    display: 'flex',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    width: '800px',
-    marginLeft: '500px',
-    marginTop: '50px',
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    width: "800px",
+    marginLeft: "13%",
+    marginTop: "50px"
   },
   button: {
-    display: 'block',
-    marginTop: theme.spacing(2),
+    display: "flex",
+    marginTop: theme.spacing(2)
   },
   formControl: {
     margin: theme.spacing(1),
-    minWidth: 120,
+    minWidth: 120
   },
   text: {
-    fontSize: '12px',
-  },
-}))
+    fontSize: "12px"
+  }
+}));
 
 export default function AdminAdd() {
-  const classes = useStyles()
-  const [location, setLocation] = React.useState('')
-  const [commerce, setCommerce] = React.useState('')
-  const [type, setType] = React.useState('')
-  const [openLocation, setOpenLocation] = React.useState(false)
-  const [openCommerce, setOpenCommerce] = React.useState(false)
-  const [openType, setOpenType] = React.useState(false)
-  const [description, setDescription] = React.useState('')
+  const classes = useStyles();
+  const [location, setLocation] = React.useState("");
+  const [commerce, setCommerce] = React.useState("");
+  const [type, setType] = React.useState("");
+  const [openLocation, setOpenLocation] = React.useState(false);
+  const [openCommerce, setOpenCommerce] = React.useState(false);
+  const [openType, setOpenType] = React.useState(false);
+  const [description, setDescription] = React.useState("");
 
-  const [mutate] = useMutation(ADD_LISTING_MUTATION)
-  const { data, loading, error } = useQuery(ADMIN_ADD_QUERY)
+  const [mutate] = useMutation(ADD_LISTING_MUTATION);
+  const { data, loading, error } = useQuery(ADMIN_ADD_QUERY);
   if (error) {
-    throw error
+    throw error;
   }
 
-  const locations = data ? data.locations : []
-  const listingTypes = data ? data.listingTypes : []
+  const locations = data ? data.locations : [];
+  const listingTypes = data ? data.listingTypes : [];
 
   function handleFormSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
     const values = {
       locationId: location,
       listingTypeId: type,
       commerce,
-      description,
-    }
+      description
+    };
 
-    console.log('values', values)
+    console.log("values", values);
 
     mutate({
       variables: {
-        input: values,
-      },
+        input: values
+      }
     }).then(output => {
-      console.log(output)
-    })
+      console.log(output);
+    });
   }
   function handleDescriptionChange(e) {
-    setDescription(e.target.value)
+    setDescription(e.target.value);
   }
 
   const handleChangeLocation = event => {
-    setLocation(event.target.value)
-  }
+    setLocation(event.target.value);
+  };
   const handleChangeCommerce = event => {
-    setCommerce(event.target.value)
-  }
+    setCommerce(event.target.value);
+  };
   const handleChangeType = event => {
-    setType(event.target.value)
-  }
+    setType(event.target.value);
+  };
 
   const handleCloseLocation = () => {
-    setOpenLocation(false)
-  }
+    setOpenLocation(false);
+  };
 
   const handleOpenLocation = () => {
-    setOpenLocation(true)
-  }
+    setOpenLocation(true);
+  };
 
   const handleCloseCommerce = () => {
-    setOpenCommerce(false)
-  }
+    setOpenCommerce(false);
+  };
 
   const handleOpenCommerce = () => {
-    setOpenCommerce(true)
-  }
+    setOpenCommerce(true);
+  };
 
   const handleCloseType = () => {
-    setOpenType(false)
-  }
+    setOpenType(false);
+  };
 
   const handleOpenType = () => {
-    setOpenType(true)
-  }
+    setOpenType(true);
+  };
 
   return (
     <div className={classes.wrapper}>
       <Router>
         <form autoComplete="off" method="post" onSubmit={handleFormSubmit}>
           <FormControl className={classes.formControl}>
-            <InputLabel htmlFor="demo-controlled-open-select">Choose Location:</InputLabel>
+            <InputLabel htmlFor="demo-controlled-open-select">
+              Location:
+            </InputLabel>
             <Select
               open={openLocation}
               onClose={handleCloseLocation}
@@ -152,22 +154,28 @@ export default function AdminAdd() {
               value={location}
               onChange={handleChangeLocation}
               inputProps={{
-                name: 'location',
-                id: 'demo-controlled-open-select',
+                name: "location",
+                id: "demo-controlled-open-select"
               }}
             >
               <MenuItem value="">
                 <em>None</em>
               </MenuItem>
               {locations.map(location => (
-                <MenuItem key={location.id} value={location.id} href="#/action-1">
+                <MenuItem
+                  key={location.id}
+                  value={location.id}
+                  href="#/action-1"
+                >
                   {location.name}
                 </MenuItem>
               ))}
             </Select>
           </FormControl>
           <FormControl className={classes.formControl}>
-            <InputLabel htmlFor="demo-controlled-open-select">Choose Commerce:</InputLabel>
+            <InputLabel htmlFor="demo-controlled-open-select">
+              Commerce:
+            </InputLabel>
             <Select
               open={openCommerce}
               onClose={handleCloseCommerce}
@@ -175,19 +183,21 @@ export default function AdminAdd() {
               value={commerce}
               onChange={handleChangeCommerce}
               inputProps={{
-                name: 'commerce',
-                id: 'demo-controlled-open-select',
+                name: "commerce",
+                id: "demo-controlled-open-select"
               }}
             >
               <MenuItem value="">
                 <em>None</em>
               </MenuItem>
-              <MenuItem value={'rent'}>Rent</MenuItem>
-              <MenuItem value={'sale'}>Sale</MenuItem>
+              <MenuItem value={"rent"}>Rent</MenuItem>
+              <MenuItem value={"sale"}>Sale</MenuItem>
             </Select>
           </FormControl>
           <FormControl className={classes.formControl}>
-            <InputLabel htmlFor="demo-controlled-open-select">Choose Type:</InputLabel>
+            <InputLabel htmlFor="demo-controlled-open-select">
+              Choose Type:
+            </InputLabel>
             <Select
               open={openType}
               onClose={handleCloseType}
@@ -195,15 +205,19 @@ export default function AdminAdd() {
               value={type}
               onChange={handleChangeType}
               inputProps={{
-                name: 'type',
-                id: 'demo-controlled-open-select',
+                name: "type",
+                id: "demo-controlled-open-select"
               }}
             >
               <MenuItem value="">
                 <em>None</em>
               </MenuItem>
               {listingTypes.map(listingType => (
-                <MenuItem key={listingType.id} value={listingType.id} href="#/action-1">
+                <MenuItem
+                  key={listingType.id}
+                  value={listingType.id}
+                  href="#/action-1"
+                >
                   {listingType.name}
                 </MenuItem>
               ))}
@@ -211,11 +225,21 @@ export default function AdminAdd() {
           </FormControl>
           <Upload />
           <p className={classes.text}>
-            Want to increase coverage location? <Link to="/addarea">Click Here!</Link>
+            Want to increase coverage location?{" "}
+            <Link to="/addarea">Click Here!</Link>
           </p>
           <InputLabel>Description:</InputLabel>
-          <textarea style={{ resize: 'none' }} value={description} onChange={handleDescriptionChange}></textarea>
-          <Button variant="outlined" color="primary" className={classes.button} type="submit">
+          <textarea
+            style={{ resize: "none" }}
+            value={description}
+            onChange={handleDescriptionChange}
+          ></textarea>
+          <Button
+            variant="outlined"
+            color="primary"
+            className={classes.button}
+            type="submit"
+          >
             Add Property
           </Button>
         </form>
@@ -226,5 +250,5 @@ export default function AdminAdd() {
         </Switch>
       </Router>
     </div>
-  )
+  );
 }
